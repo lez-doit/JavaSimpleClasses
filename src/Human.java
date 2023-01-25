a998d1cbbb785fdb35f33abd63d357f3cd141cff
Artem Popov <br4v0.2403@gmail.com>
2023-01-25 10:51:08 +0200

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,16 @@
+public class Car
+{
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 40000.0;
+
+    void drive() {
+        System.out.println("You`re driving the car.");
+    }
+
+    void brake() {
+        System.out.println("You`re stepped on brakes.");
+    }
+}
